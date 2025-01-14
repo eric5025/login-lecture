8433d5d2ba2e5ff22f278d00f6d6788a1d7dfa8b
eric5025 <kweon5025@gmail.com>
2025-01-14 11:24:12 +0900

    프런트 단 javascript 파일만들기

--- app/app.js
+++ app/app.js
@@ -11,6 +11,7 @@ const home = require("./src/routes/home"); // 라우팅
 app.set("views","./src/views");
 app.set("view engine","ejs");
 
+app.use(express.static(__dirname+'/src/public')) //왜이러냐 $빼니까되네네
 
 app.use("/", home); //미들웨어를 등록하는 메서드
 
